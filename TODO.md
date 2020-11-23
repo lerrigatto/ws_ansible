9b80d61eb9de121b6b2275fb39183261886f0c12
Enrico lerrigatto La Cava <me@lerrigatto.com>
2020-11-23 12:09:35 +0100

    chore: fix ansible scripts

--- TODO.md
+++ TODO.md
@@ -1,2 +1,2 @@
 # TODO list
-
+- install xclip in desktop
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$HOME/.local/bin/ansible-playbook -i inventories desktop_env.yml -v --connection=local --ask-become-pass
+$HOME/.local/bin/ansible-playbook -i inventories desktop_env.yml -v --connection=local --ask-become-pass $1
